282216f92bd8c5977e5f81bee37f9d09fd572334
Luis Gustavo Furtuozo Falci <luisgustavo.1999@hotmail.com>
2023-09-05 01:14:12 -0300

    solved version

--- app/src/main/java/com/example/bluetooth/MainActivity.kt
+++ app/src/main/java/com/example/bluetooth/MainActivity.kt
@@ -178,6 +178,7 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.S)
     private fun enableDisableBluetooth() {
         binding.btONOFF.setOnClickListener {
+            checkPermissions()
             if (!bluetoothAdapter.isEnabled) {
                 bluetoothAdapter.enable()
                 val intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
@@ -192,6 +193,35 @@ class MainActivity : AppCompatActivity() {
                 val intentFilter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
                 registerReceiver(receiver, intentFilter)
             }
+        }
+    }
+
+    private val PERMISSIONS_STORAGE = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,
+        Manifest.permission.BLUETOOTH_SCAN,
+        Manifest.permission.BLUETOOTH_CONNECT,
+        Manifest.permission.BLUETOOTH_PRIVILEGED
+    )
+    private val PERMISSIONS_LOCATION = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,
+        Manifest.permission.BLUETOOTH_SCAN,
+        Manifest.permission.BLUETOOTH_CONNECT,
+        Manifest.permission.BLUETOOTH_PRIVILEGED
+    )
+
+    private fun checkPermissions() {
+        val permission1 = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val permission2 = ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN)
+        if (permission1 != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(this,PERMISSIONS_STORAGE,1)
+        } else if (permission2 != PackageManager.PERMISSION_GRANTED) {ActivityCompat.requestPermissions(this,PERMISSIONS_LOCATION,1)
         }
     }
 
